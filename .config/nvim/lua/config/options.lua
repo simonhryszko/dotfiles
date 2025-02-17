0e0747760f6218782f5cd787454e0e08c4accc75
Simon Hryszko <simonhryszko@gmail.com>
2025-02-17 10:00:00 +0100

    Automated backup Mon Feb 17 10:00:00 AM CET 2025

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- Set scrolloff to a large value to keep the cursor centered
+vim.opt.scrolloff = 999
