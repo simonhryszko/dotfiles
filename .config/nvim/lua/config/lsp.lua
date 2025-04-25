4d698f4285aae3b76b0b84df3d208cab322942ba
Simon Hryszko <simonhryszko@gmail.com>
2025-04-25 12:00:00 +0200

    Automated backup Fri Apr 25 12:00:00 PM CEST 2025

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,23 +1,3 @@
-return {
-  servers = {
-    pylsp = {
-      settings = {
-        pylsp = {
-          plugins = {
-            autopep8 = {
-              enabled = false, -- disable autopep8
-              ignore = { "E203" }, -- Disable the leading comma rule (E203)
-            },
-            pycodestyle = {
-              enabled = false, -- disable autopep8
-              ignore = { "E501" },
-            },
-            pyflakes = {
-              enabled = false,
-            },
-          },
-        },
-      },
-    },
-  },
+require("lazyvim.config.lsp").formatting = {
+  autopep8 = { enabled = true },
 }
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,5 +3,7 @@
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
 
+vim.g.lazyvim_python_lsp = "autopep8"
+--vim.g.lazyvim_python_ruff = "autopep8"
 -- Set scrolloff to a large value to keep the cursor centered
 vim.opt.scrolloff = 999
