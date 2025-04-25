63cb60f8739b5049e0dda67716b62860c8999dc1
Simon Hryszko <simonhryszko@gmail.com>
2025-04-25 11:00:00 +0200

    Automated backup Fri Apr 25 11:00:00 AM CEST 2025

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -5,11 +5,15 @@ return {
         pylsp = {
           plugins = {
             autopep8 = {
-              enabled = true, -- Enable autopep8
+              enabled = false, -- disable autopep8
               ignore = { "E203" }, -- Disable the leading comma rule (E203)
             },
             pycodestyle = {
+              enabled = false, -- disable autopep8
               ignore = { "E501" },
+            },
+            pyflakes = {
+              enabled = false,
             },
           },
         },
