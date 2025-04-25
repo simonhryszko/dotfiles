a9a8a23f6bd21ada3690509ee6af94490dbb163e
Simon Hryszko <simonhryszko@gmail.com>
2025-04-25 14:00:00 +0200

    Automated backup Fri Apr 25 02:00:00 PM CEST 2025

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,6 +1,6 @@
 -- Configure LazyVim to use conform.nvim for formatting
-local lsp_config = require("lazyvim.config.lsp")
+-- local lsp_config = require("lazyvim.config.lsp")
 
 -- Disable the LSP formatting capability to let conform.nvim handle it
-lsp_config.capabilities.documentFormattingProvider = false
-lsp_config.capabilities.documentRangeFormattingProvider = false
+-- lsp_config.capabilities.documentFormattingProvider = false
+-- lsp_config.capabilities.documentRangeFormattingProvider = false
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,4 +6,4 @@ vim.opt.winbar = "%=%m %f"
 --vim.g.lazyvim_python_lsp = "pyright" -- Use pyright as the Python LSP
 --vim.g.lazyvim_python_formatter = "autopep8" -- Use autopep8 as the formatter
 -- Set scrolloff to a large value to keep the cursor centered
-vim.opt.scrolloff = 999
+--vim.opt.scrolloff = 999
