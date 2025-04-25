f605e89aed4d7548309cb448b2733e59b9604b9c
Simon Hryszko <simonhryszko@gmail.com>
2025-04-25 13:00:00 +0200

    Automated backup Fri Apr 25 01:00:00 PM CEST 2025

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,3 +1,6 @@
-require("lazyvim.config.lsp").formatting = {
-  autopep8 = { enabled = true },
-}
+-- Configure LazyVim to use conform.nvim for formatting
+local lsp_config = require("lazyvim.config.lsp")
+
+-- Disable the LSP formatting capability to let conform.nvim handle it
+lsp_config.capabilities.documentFormattingProvider = false
+lsp_config.capabilities.documentRangeFormattingProvider = false
