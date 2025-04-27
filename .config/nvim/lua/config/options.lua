1e413456daec963b56c2b778814c8752e5714067
Simon Hryszko <simonhryszko@gmail.com>
2025-04-27 13:00:00 +0200

    Automated backup Sun Apr 27 01:00:00 PM CEST 2025

--- .config/nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.winbar = "%=%m %f"
-
---vim.g.lazyvim_python_lsp = "pyright" -- Use pyright as the Python LSP
---vim.g.lazyvim_python_formatter = "autopep8" -- Use autopep8 as the formatter
--- Set scrolloff to a large value to keep the cursor centered
---vim.opt.scrolloff = 999
